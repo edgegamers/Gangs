0d612dc92cbe09f4faec42a21e1af69b052c52b2
MSWS <imodmaker@gmail.com>
2024-09-11 21:35:41 -0700

    Move tostring into actual datas

--- src/GangsImpl/Stats/Stat/Player/KDRStat.cs
+++ src/GangsImpl/Stats/Stat/Player/KDRStat.cs
@@ -6,17 +6,6 @@ public class KDRStat : BaseStat<KDRData> {
   public override string StatId => "gang_native_kdr";
   public override string Name => "KDR";
   public override string? Description => "Kill-Death Ratio";
-
-  public override string ToString() {
-    if (Value == null) return $"N/A";
-    var deaths = Value.TDeaths + Value.CTDeaths;
-    if (deaths == 0) return $"{ChatColors.LightRed}{Value.TKills}";
-    var line =
-      $"CT KDR: {Value.CTKills}/{Value.CTDeaths} ({Value.CTKills / Value.CTDeaths:F2})\n";
-    line +=
-      $"T KDR: {Value.TKills}/{Value.TDeaths} ({Value.TKills / Value.TDeaths:F2})";
-    return line;
-  }
 }
 
 public class KDRData {
@@ -24,4 +13,12 @@ public class KDRData {
   public int TDeaths { get; set; }
   public int CTKills { get; set; }
   public int CTDeaths { get; set; }
+
+  public override string ToString() {
+    var deaths = TDeaths + CTDeaths;
+    if (deaths == 0) return $"{ChatColors.LightRed}{TKills}\n{CTKills}";
+    var line = $"CT KDR: {CTKills}/{CTDeaths} ({CTKills / CTDeaths:F2})\n";
+    line += $"T KDR: {TKills}/{TDeaths} ({TKills / TDeaths:F2})";
+    return line;
+  }
 }
--- src/GangsImpl/Stats/Stat/Player/RoundStats.cs
+++ src/GangsImpl/Stats/Stat/Player/RoundStats.cs
@@ -4,16 +4,14 @@ public class RoundStats : BaseStat<RoundData> {
   public override string StatId => "gang_native_rounds";
   public override string Name => "Round Stats";
   public override string Description => "Rounds won, lost and MVPs";
-
-  public override string ToString() {
-    if (Value == null) return $"N/A";
-    return "(Wins/Losses/MVPs): "
-      + $"{Value.RoundsWon}/{Value.RoundsLost}/{Value.RoundsMVP}";
-  }
 }
 
 public class RoundData {
   public int RoundsWon { get; set; }
   public int RoundsLost { get; set; }
   public int RoundsMVP { get; set; }
+
+  public override string ToString() {
+    return "(Wins/Losses/MVPs): " + $"{RoundsWon}/{RoundsLost}/{RoundsMVP}";
+  }
 }
