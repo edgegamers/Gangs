226467701dca046f7150b53c9c8a3c4300e8ed18
MSWS <imodmaker@gmail.com>
2024-10-30 16:39:11 -0700

    More work

--- src/CS2/Leaderboard/LeaderboardCommand.cs
+++ src/CS2/Leaderboard/LeaderboardCommand.cs
@@ -3,23 +3,27 @@ using CounterStrikeSharp.API.Core.Attributes.Registration;
 using GangsAPI.Data;
 using GangsAPI.Data.Command;
 using GangsAPI.Services.Commands;
+using GangsAPI.Services.Gang;
 
 namespace Leaderboard;
 
-public class LeaderboardCommand(ILeaderboard leaderboard) : ICommand {
+public class LeaderboardCommand(ILeaderboard leaderboard, IGangManager gangs)
+  : ICommand {
   public string Name => "css_gangrank";
   public string[] Aliases => ["css_gangranks", "css_gangtop", "css_ganglb"];
   private (int, double)[]? cachedLeaderboard;
 
   public async Task<CommandResult> Execute(PlayerWrapper? executor,
     CommandInfoWrapper info) {
-    info.ReplySync("Fetching leaderboard...");
     cachedLeaderboard ??= (await leaderboard.GetTopGangs()).ToArray();
 
-    foreach (var (gangId, score) in cachedLeaderboard)
-      info.ReplySync($"Gang {gangId} has a score of {score}");
+    var index = 0;
+    foreach (var (gangId, score) in cachedLeaderboard) {
+      var gang = await gangs.GetGang(gangId);
+      if (gang == null) continue;
+      info.ReplySync($"{++index} {gang.Name}: {score}");
+    }
 
-    info.ReplySync($"{cachedLeaderboard.Length} total ranked gangs.");
     return CommandResult.SUCCESS;
   }
 
