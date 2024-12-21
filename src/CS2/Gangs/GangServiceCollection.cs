d825f25f00e5f8d4911734e6cde345b6512e78b4
MSWS <imodmaker@gmail.com>
2024-12-21 03:56:10 -0800

    Register

--- src/CS2/Gangs/GangServiceCollection.cs
+++ src/CS2/Gangs/GangServiceCollection.cs
@@ -29,6 +29,7 @@ public class GangServiceCollection : IPluginServiceCollection<CS2Gangs> {
     serviceCollection.AddPluginBehavior<IPlayerManager, MySQLPlayerManager>();
     serviceCollection
      .AddPluginBehavior<IMenuManager, CommandBasedMenuManager>();
+    serviceCollection.AddPluginBehavior<ZeroCommand>();
     serviceCollection.AddPluginBehavior<IStatManager, StatManager>();
     serviceCollection
      .AddPluginBehavior<IGangStatManager, MySQLGangInstanceManager>();
