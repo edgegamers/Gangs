dfbb1a51f763bf4d986a571ebdc6dfd14887333a
MSWS <imodmaker@gmail.com>
2025-02-23 18:39:22 -0800

    Add webhook

---
+++ src/CS2/Gangs/EnvWebhookConfig.cs
@@ -0,0 +1,9 @@
+﻿using GangsAPI.Data;
+
+namespace GangsImpl;
+
+public class EnvWebhookConfig : IWebhookConfig {
+  public string WebhookUrl
+    => Environment.GetEnvironmentVariable("WEBHOOK_GANGS_URL")
+      ?? "https://discord.com/api/webhooks/123456789012345678/abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+}
